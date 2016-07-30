0ba28e79b93f1902e183e7c18e6b9bcb56c2ee4e
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-07-30 09:24:05 +0200

    feat: Day 1 Solution
    Solution with java.

--- java8/day1/Solution.java
+++ java8/day1/Solution.java
@@ -13,16 +13,21 @@ public class Solution {
         String s = "HackerRank ";
 
         Scanner scan = new Scanner(System.in);
-        /* Declare second integer, double, and String variables. */
 
-        /* Read and save an integer, double, and String to your variables.*/
+        i += scan.nextInt();
+        d += scan.nextDouble();
+        scan.nextLine();
+        s += scan.nextLine();
 
         /* Print the sum of both integer variables on a new line. */
-
+        System.out.println(i);
         /* Print the sum of the double variables on a new line. */
+        System.out.println(d);
 
         /* Concatenate and print the String variables on a new line;
         	the 's' variable above should be printed first. */
+        System.out.println(s);
+
         scan.close();
     }
 }
