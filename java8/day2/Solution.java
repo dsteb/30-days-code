41e833cf59decfcdc0b95e4f9c88f2a57f68a1db
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-07-31 17:16:41 +0200

    refactor: Day 2 Final solution
    Use autoclose for closeable resources.

--- java8/day2/Solution.java
+++ java8/day2/Solution.java
@@ -8,20 +8,20 @@ import java.util.regex.*;
 public class Arithmetic {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        double mealCost = scan.nextDouble(); // original meal price
-        int tipPercent = scan.nextInt(); // tip percentage
-        int taxPercent = scan.nextInt(); // tax percentage
-        scan.close();
+        try (Scanner scan = new Scanner(System.in)) {
+            double mealCost = scan.nextDouble(); // original meal price
+            int tipPercent = scan.nextInt(); // tip percentage
+            int taxPercent = scan.nextInt(); // tax percentage
 
-        // Write your calculation code here.
-        double tip = mealCost * tipPercent / 100;
-        double tax = mealCost * taxPercent / 100;
+            // Write your calculation code here.
+            double tip = mealCost * tipPercent / 100;
+            double tax = mealCost * taxPercent / 100;
 
-        // cast the result of the rounding operation to an int and save it as totalCost
-        int totalCost = (int) Math.round(mealCost + tip + tax);
+            // cast the result of the rounding operation to an int and save it as totalCost
+            int totalCost = (int) Math.round(mealCost + tip + tax);
 
-        // Print your result
-        System.out.printf("The total meal cost is %d dollars.", totalCost);
+            // Print your result
+            System.out.printf("The total meal cost is %d dollars.", totalCost);
+        }
     }
 }
